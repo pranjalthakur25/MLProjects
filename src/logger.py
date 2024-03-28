89491b981e982cbb551e4f29743531d33649f199
Pranjal <pranjalthakur252003@gmail.com>
2024-03-28 23:48:14 +0530

    code added

---
+++ src/logger.py
@@ -0,0 +1,19 @@
+# Logging is a means of tracking events that happen 
+# when some software runs. Logging is important for 
+# software developing, debugging, and running. 
+import logging
+import os
+from datetime import datetime
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
+os.makedirs(logs_path,exist_ok=True)
+
+LOG_FILE_PATH=os.path.join(logs_path, LOG_FILE)
+
+logging.basicConfig(
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+if __name__=="__main__":
+    logging.info("Logging has started")
---
+++ src/utils.py
@@ -0,0 +1,2 @@
+# Utility files (often named utils.py) contain reusable 
+# functions or classes that serve specific purposes across your project.
